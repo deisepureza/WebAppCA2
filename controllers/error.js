49c825e5644b1e510c407620209e7e5b740931de
deisepureza <61194083+deisepureza@users.noreply.github.com>
2021-05-01 18:31:39 +0100

    storing data in file/applaying MVC

---
+++ controllers/error.js
@@ -0,0 +1,4 @@
+//this function will return the 404 page
+exports.get404 = (req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  };
--- routes/admin.js
+++ routes/admin.js
@@ -2,29 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../helper/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  //expressjs render method that will get the view engine in app.js
-  res.render('add-product', { //defining object that will hold the data and will be passed as variables
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
