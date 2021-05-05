6110d495989dbcdc19af70f988315a10a302dd49
deisepureza <61194083+deisepureza@users.noreply.github.com>
2021-05-05 16:41:17 +0100

    fixing validation err

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,12 +1,29 @@
+//importing validation result from the package
+const {validationResult} = require('express-validator/check');
+
 //exporting function (will return a json response)
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{title: 'My First Post', content: 'This is my first post!'}]
+        posts: [{title: 'My First Post', 
+        content: 'This is my first post!', 
+        imageUrl: 'img/LEMONADE.png',
+        creator: {
+            name:'Deisiane'
+        },
+        createdAt: new Date() 
+        }]
     });
 };
 
 //post router
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422)
+            .json({message: 'Something was incorret, Validation failed.',
+            errors: errors.array()
+        });
+    }
     const title = req.body.title;
     const content = req.body.content;
     console.log(title, content);
@@ -14,6 +31,12 @@ exports.createPost = (req, res, next) => {
     //setting status method to 201 (tell the client  the resource was created success) / 200 (sucesso only)
     res.status(201).json({
         message: 'Your Post was created successfully!',
-        post: {id: new Date().toISOString(), title: title, content: content}
+        post: {
+            _id: new Date().toISOString(), 
+            title: title, 
+            content: content, 
+            creator:{ name: 'Deisiane' },
+            createdAt: new Date()
+        }
     });
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,5 +1,7 @@
 //setting express router 
 const express = require('express');
+// importing from express validator package
+const { body } = require('express-validator/check');
 
 //importng feed controllers
 const feedController = require('../controllers/feed');
@@ -13,7 +15,14 @@ const router = express.Router();
 router.get('/posts', feedController.getPosts);
 
 //POST //feed/post
-router.post('/post', feedController.createPost);
+router.post('/post',
+ [
+    body('title')
+        .trim()
+        .isLength({min: 5}),
+    body('content').trim().isLength({min: 5})
+ ],
+ feedController.createPost);
 
 //exporting the router 
 module.exports = router;
